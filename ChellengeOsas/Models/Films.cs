b743199406d147afced78e1ec5628a4bb65a8f0a
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-09 15:59:38 -0300

    Create Films.cs

---
+++ ChellengeOsas/Models/Films.cs
@@ -0,0 +1,12 @@
+namespace ChellengeOsas.Models
+{
+    public class Films
+    {
+        public string Title { get; set; }
+        public string Model { get; set; }
+        public int Length { get; set; }
+        public int Passengers { get; set; }
+
+
+    }
+}
