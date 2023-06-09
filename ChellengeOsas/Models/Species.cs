91b1ddb42344cca4c4171c31c40c0c44f690dacb
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-09 16:02:28 -0300

    Create Species.cs

---
+++ ChellengeOsas/Models/Species.cs
@@ -0,0 +1,10 @@
+namespace ChellengeOsas.Models
+{
+    public class Species
+    {
+        public string Name { get; set; }
+        public string Classification { get; set; }
+        public int Height { get; set; }
+        public string Colors { get; set; }
+    }
+}
