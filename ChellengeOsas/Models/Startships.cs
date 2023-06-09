cd7d4a03804c474b44e9b1b583864d2665118155
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-09 16:02:59 -0300

    Create Startships.cs

---
+++ ChellengeOsas/Models/Startships.cs
@@ -0,0 +1,10 @@
+namespace ChellengeOsas.Models
+{
+    public class Starships
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Length { get; set; }
+        public string Passengers { get; set; }
+    }
+}
