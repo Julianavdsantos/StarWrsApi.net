d52339b4b1736f975bb4f2a8f89f00208ae35b96
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-09 16:00:28 -0300

    Update and rename starWars.cs to people.cs

---
+++ ChellengeOsas/Models/people.cs
@@ -0,0 +1,10 @@
+namespace ChellengeOsas.Models
+{
+    public class Peoples
+    {
+        public string Nomes { get; set; }
+        public int Height { get; set; }
+        public int Birth { get; set; }
+        public string Gender { get; set; }
+    }
+}
