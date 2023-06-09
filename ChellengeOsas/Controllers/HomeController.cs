882cb31972ff7ecab38dd0d7cb283f45009232e6
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-09 16:04:08 -0300

    Update HomeController.cs

--- ChellengeOsas/Controllers/HomeController.cs
+++ ChellengeOsas/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-﻿using ChellengeOsas.Models;
+using ChellengeOsas.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -37,20 +35,24 @@ namespace ChellengeOsas.Controllers
                     {
                         string json = reader.ReadToEnd();
 
-                        var data = JsonConvert.DeserializeObject<starWars>(json);
+                        var films = JsonConvert.DeserializeObject<Films>(json);
+                        var peoples = JsonConvert.DeserializeObject<Peoples>(json);
+                        var planets = JsonConvert.DeserializeObject<Planets>(json);
+                        var species = JsonConvert.DeserializeObject<Species>(json);
+                        var starships = JsonConvert.DeserializeObject<Starships>(json);
+                        var vehicles = JsonConvert.DeserializeObject<Vehicles>(json);
 
-                        ViewBag.JsonData = JsonConvert.SerializeObject(data);
+                        ViewBag.Films = films;
+                        ViewBag.Peoples = peoples;
+                        ViewBag.Planets = planets;
+                        ViewBag.Species = species;
+                        ViewBag.Starships = starships;
+                        ViewBag.Vehicles = vehicles;
 
-                        return View(data);
-
-                       
-
+                        return View();
                     }
                 }
             }
-
-                   
-
         }
 
         public IActionResult Privacy()
@@ -58,10 +60,33 @@ namespace ChellengeOsas.Controllers
             return View();
         }
 
-
-
-
-
+        [HttpPost]
+        public IActionResult GetData(string type)
+        {
+            switch (type)
+            {
+                case "films":
+                    var films = ViewBag.Films as Films;
+                    return PartialView("_FilmsPartialView.cshtml", films);
+                case "people":
+                    var peoples = ViewBag.Peoples as Peoples;
+                    return PartialView("_PeoplePartialView.cshtml", peoples);
+                case "planets":
+                    var planets = ViewBag.Planets as Planets;
+                    return PartialView("_PlanetsPartialView.cshtml", planets);
+                case "species":
+                    var species = ViewBag.Species as Species;
+                    return PartialView("_SpeciesPartialView.cshtml", species);
+                case "starships":
+                    var starships = ViewBag.Starships as Starships;
+                    return PartialView("_StarshipsPartialView.cshtml", starships);
+                case "vehicles":
+                    var vehicles = ViewBag.Vehicles as Vehicles;
+                    return PartialView("_VehiclesPartialView.cshtml", vehicles);
+                default:
+                    return PartialView();
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
