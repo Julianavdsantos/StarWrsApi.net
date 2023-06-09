d74dd4aaa06a8f7fa70303ab757b2f53c3709a38
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-09 16:03:24 -0300

    Create Vehicles.cs

---
+++ ChellengeOsas/Models/Vehicles.cs
@@ -0,0 +1,10 @@
+namespace ChellengeOsas.Models
+{
+    public class Vehicles
+    {
+        public string Nome { get; set; }
+        public string Model { get; set; }
+        public int Length { get; set; }
+        public int Passengers { get; set; }
+    }
+}
